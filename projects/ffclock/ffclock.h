42881927f965bdbe597164d74d56af5eb9db270d
Philippe Kehl <phkehl@gmail.com>
2017-04-29 15:37:46 +0200

    ffclock: more docs

--- projects/ffclock/ffclock.h
+++ projects/ffclock/ffclock.h
@@ -14,6 +14,7 @@
     - [dialight_547-2xxx.pdf](../../projects/ffclock/docs/dialight_547-2xxx.pdf) (LED modules)
     - [helical_antenna.pdf](../../projects/ffclock/docs/helical_antenna.pdf) (from http://home.iae.nl/users/plundahl/antenne/helical.htm, https://web.archive.org/web/20091210100330/http://home.iae.nl/users/plundahl/antenne/helical.htm)
     - [LN2003A.pdf](../../projects/ffclock/docs/LN2003A.pdf) (darlington transistor array)
+    - [L293.pdf](../../projects/ffclock/docs/L293.pdf) (driver)
     - [panasonic_av4444.pdf](../../projects/ffclock/docs/panasonic_av4444.pdf) (mini switches)
     - [NEO-M8_DataSheet_(UBX-13003366).pdf](../../projects/ffclock/docs/NEO-M8_DataSheet_(UBX-13003366).pdf) (GNSS receiver)
     - [NEO-M8_HardwareIntegrationManual_(UBX-13003557).pdf](../../projects/ffclock/docs/NEO-M8_HardwareIntegrationManual_(UBX-13003557).pdf) (GNSS receiver)

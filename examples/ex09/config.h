b632d80042546aec2030fc1b4f738cd485fc3988
Philippe Kehl <phkehl@gmail.com>
2017-06-09 14:33:22 +0200

    example 9 empty skeletton

---
+++ examples/ex09/config.h
@@ -0,0 +1,25 @@
+/*!
+    \file
+    \brief flipflip's Arduino Uno stuff: eighth example application (see \ref EXAMPLES_EX08)
+
+    - Copyright (c) 2017 Philippe Kehl (flipflip at oinkzwurgl dot org)
+
+    \addtogroup EXAMPLES_EX08
+    @{
+*/
+
+#ifndef __APP_CONFIG_H__
+#define __APP_CONFIG_H__
+
+/* *************************************************************************** */
+
+
+// complete configuration by including the defaults
+#include "defconfig.h"
+
+
+/* *************************************************************************** */
+
+#endif // __APP_CONFIG_H__
+//@}
+// eof

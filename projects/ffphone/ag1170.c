8aee7c8e31560bfa67065c1b30445af97bcef000
Philippe Kehl <phkehl@gmail.com>
2017-06-12 22:54:33 +0200

    Ag1170 Ringing SLIC docu

---
+++ projects/ffphone/ag1170.c
@@ -0,0 +1,19 @@
+/*!
+    \file
+    \brief flipflip's Arduino Uno stuff: Silvertel Ag1170 low power ringing SLIC module (see \ref PROJECTS_FFPHONE_AG1170)
+
+    - Copyright (c) 2011-2017 Philippe Kehl (flipflip at oinkzwurgl dot org)
+
+    \addtogroup PROJECTS_FFPHONE_AG1170
+    @{
+*/
+
+#include "stdstuff.h"      // ff: useful macros and types
+
+
+/* *************************************************************************** */
+
+
+/* *************************************************************************** */
+//@}
+// eof
---
+++ projects/ffphone/ag1170.h
@@ -0,0 +1,31 @@
+/*!
+    \file
+    \brief flipflip's Arduino Uno stuff: Silvertel Ag1170 Low Power Ringing SLIC module (see \ref PROJECTS_FFPHONE_AG1170)
+
+    - Copyright (c) 2011-2017 Philippe Kehl (flipflip at oinkzwurgl dot org)
+
+    \defgroup PROJECTS_FFPHONE_AG1170 AG1170
+    \ingroup PROJECTS_FFPHONE
+
+    Stuff for the Silvertel Ag1170 Low Power Ringing SLIC (subscriber line interface circuit).
+
+    - [AG1170 Datasheet](../../projects/ffphone/ag1170_datasheet.pdf)
+
+    @{
+*/
+
+#ifndef __AG1170_H__
+#define __AG1170_H__
+
+
+#include "stdstuff.h"      // ff: useful macros and types
+
+
+/* *************************************************************************** */
+
+
+/* *************************************************************************** */
+
+#endif // __AG1170_H__
+// @}
+// eof

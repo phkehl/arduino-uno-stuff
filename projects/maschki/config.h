55492d3fb807b254de46e10f2005ef56d4489dc1
Philippe Kehl <phkehl@gmail.com>
2017-09-10 19:39:49 +0200

    maschki: low-level code ported

--- projects/maschki/config.h
+++ projects/maschki/config.h
@@ -14,12 +14,17 @@
 /* *************************************************************************** */
 
 
-// use the built-in LED for the system load and status
-#define FF_HW_LOAD_PIN _D13
-
 // gain some memory by disabling serial port receive
 #define FF_HW_RX_BUFSIZE 0
 
+// we have this many LEDs
+#define FF_LEDFX_NUM_LED 2
+
+// GRB colour ordering
+#define FF_LEDFX_ORDER 123
+
+// precise time measurement
+#define FF_HW_NUM_TICTOC 1
 
 // complete configuration by including the defaults
 #include "defconfig.h"
--- projects/maschki/maschki.c
+++ projects/maschki/maschki.c
@@ -18,8 +18,8 @@
 #include "os.h"            // ff: operating system abstractions
 #include "hw.h"            // ff: hardware abstraction
 #include "sys.h"           // ff: system task
-#include "hsv2rgb.h"       // ff: HSV to RGV conversion
 #include "ws2801.h"        // ff: WS2811 LED driver
+#include "ledfx.h"         // ff: LED effects
 
 #include "maschki.h"
 
@@ -30,10 +30,34 @@
 static void sAppStatus(char *str, const size_t size);
 static void sAppTask(void *pArg);
 
+static OS_SEMAPHORE_t sIsrActivitySem;
+static OS_SEMAPHORE_t sIsrRangeSem;
+
 // initialise the user application
 void appInit(void)
 {
     DEBUG("maschki: init");
+
+    // RGB LEDs
+    ws2801Init();
+
+    // PIR sensor
+    PIN_INPUT(_D3);
+    PIN_PULLUP_OFF(_PD3);
+    osSemaphoreCreate(&sIsrActivitySem, 0);
+    EICRA |= BIT(ISC11) | BIT(ISC10);   // raising edge triggers INT1
+    EIMSK |= BIT(INT1);                 // enable INT1 interrupt
+    EIFR  |= BIT(INTF1);                // clear interrupt
+
+    // range sensor
+    PIN_OUTPUT(_D4);
+    PIN_LOW(_D4);
+    PIN_INPUT(_D2);
+    PIN_PULLUP_OFF(_D2);
+    osSemaphoreCreate(&sIsrRangeSem, 0);
+    EICRA |= BIT(ISC00);   // any logical change triggers INT0
+    //EIMSK |= BIT(INT0);    // enable INT0
+    EIFR  |= BIT(INTF0);   // clear interrupt
 
     // register status function for the system task
     sysRegisterMonFunc(sAppStatus);
@@ -50,6 +74,38 @@ void appCreateTask(void)
 
 /* ***** application functions *********************************************** */
 
+// PIR sensor activity
+ISR(INT1_vect)
+{
+    osIsrEnter();
+    osSemaphoreGive(&sIsrActivitySem, true);
+    osIsrLeave();
+}
+
+static volatile uint16_t svRangeRaw;
+
+// range measurement
+ISR(INT0_vect)
+{
+    osIsrEnter();
+
+    // raising edge: start measurement
+    if (svRangeRaw == 0)
+    {
+        hwTic(0);
+        svRangeRaw++;
+    }
+    // falling edge: stop measurement
+    else
+    {
+        svRangeRaw = hwToc(0);
+        osSemaphoreGive(&sIsrRangeSem, true);
+        CLRBITS(EIMSK, BIT(INT0));
+    }
+
+    osIsrLeave();
+}
+
 
 /* ***** application task **************************************************** */
 
@@ -65,11 +121,61 @@ static void sAppTask(void *pArg)
 
     //CS_ENTER; while (1) {}; CS_LEAVE;
 
+    static uint8_t sHue = 0;
+    ledfxSetBrightness(50);
+
+    static uint8_t rangeCnt;
     while (ENDLESS)
     {
-        DEBUG("maschki..");
-        osTaskDelay(1234);
+        //osTaskDelay(50);
 
+        if (osSemaphoreTake(&sIsrActivitySem, 50))
+        {
+            DEBUG("PIR");
+        }
+
+        rangeCnt++;
+        if (rangeCnt > 10)
+        {
+            rangeCnt = 0;
+            //DEBUG("range");
+
+            // trigger range measurement
+            PIN_LOW(_D4);
+            PIN_HIGH(_D4);
+            hwDelay(10);
+            PIN_LOW(_D4);
+
+            // arm interrupt to measure time
+            svRangeRaw = 0;
+            SETBITS(EIMSK, BIT(INT0));
+
+            // wait for result
+            if (osSemaphoreTake(&sIsrRangeSem, 50))
+            {
+                DEBUG("range=%"PRIu16, svRangeRaw);
+            }
+            // timeout
+            else
+            {
+                WARNING("range to %"PRIu16, svRangeRaw);
+                CLRBITS(EIMSK, BIT(INT0));
+            }
+
+
+            //osTaskDelay(1);
+            //hwTic(0);
+            //osTaskDelay(2);
+            //DEBUG("delay=%"PRIu16, hwToc(0));
+        }
+
+        // sweep hue value
+        ledfxSetIxHSV(0, sHue,       255, 255);
+        ledfxSetIxHSV(1, sHue + 128, 255, 255);
+        sHue++;
+
+        // write data to the LEDs
+        ws2801Send(ledfxGetFrameBuffer(), ledfxGetFrameBufferSize());
     }
 }
 

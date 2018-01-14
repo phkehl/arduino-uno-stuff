66ac40b31d09522007cef401f301f495266689d0
Philippe Kehl <phkehl@gmail.com>
2018-01-14 18:50:15 +0100

    doxygen stfu

--- projects/ffmatrix8x8b/ffmatrix8x8b.c
+++ projects/ffmatrix8x8b/ffmatrix8x8b.c
@@ -39,8 +39,10 @@ void appCreateTask(void)
     osTaskCreate("snd", 7, &task, stack, sizeof(stack), sAppTask, NULL);
 }
 
+#if !defined(__DOXYGEN__)
+
 // ../../tools/rtttl2melody.pl "HappyBday:d=4,32p,o=5,32p,b=125:8d.,32p,16d,32p,e,32p,d,32p,g,32p,2f#,32p,8d.,32p,16d,32p,e,32p,d,32p,a,32p,2g,32p,8d.,32p,16d,32p,d6,32p,b,32p,g,32p,f#,32p,2e,32p,8c.6,32p,16c6,32p,b,32p,g,32p,a,32p,2g"
-//! HappyBday melody
+// HappyBday melody
 static const uint16_t skMelodyHappybday[] PROGMEM =
 {
     TONE_NOTE_D5, 360, TONE_PAUSE, 60, TONE_NOTE_D5, 120, TONE_PAUSE, 60, TONE_NOTE_E5, 480,
@@ -56,7 +58,7 @@ static const uint16_t skMelodyHappybday[] PROGMEM =
 };
 
 // ../../tools/rtttl2melody.pl "Bond:d=4,o=5,b=80:32p,16c#6,32d#6,32d#6,16d#6,8d#6,16c#6,16c#6,16c#6,16c#6,32e6,32e6,16e6,8e6,16d#6,16d#6,16d#6,16c#6,32d#6,32d#6,16d#6,8d#6,16c#6,16c#6,16c#6,16c#6,32e6,32e6,16e6,8e6,16d#6,16d6,16c#6,16c#7,c.7,16g#6,16f#6,g#.6"
-//! Bond melody
+// Bond melody
 static const uint16_t skMelodyBond[] PROGMEM =
 {
     TONE_PAUSE, 94, TONE_NOTE_CS5, 188, TONE_NOTE_DS5, 94, TONE_NOTE_DS5, 94, TONE_NOTE_DS5, 188,
@@ -70,7 +72,7 @@ static const uint16_t skMelodyBond[] PROGMEM =
 };
 
 // ../../tools/rtttl2melody.pl "ATeam:d=8,o=5,b=125:4d#6,a#,2d#6,16p,g#,4a#,4d#.,p,16g,16a#,d#6,a#,f6,2d#6,16p,c#.6,16c6,16a#,g#.,2a#"
-//! ATeam melody
+// ATeam melody
 static const uint16_t skMelodyAteam[] PROGMEM =
 {
     TONE_NOTE_DS5, 480, TONE_NOTE_AS5, 240, TONE_NOTE_DS5, 960, TONE_PAUSE, 120, TONE_NOTE_GS5, 240,
@@ -80,7 +82,7 @@ static const uint16_t skMelodyAteam[] PROGMEM =
 };
 
 // ../../tools/rtttl2melody.pl "BeethovenElise:d=8,o=5,b=125:32p,e6,d#6,e6,d#6,e6,b,d6,c6,4a.,c,e,a,4b.,e,g#,b,4c.6,e,e6,d#6,e6,d#6,e6,b,d6,c6,4a.,c,e,a,4b.,d,c6,b,2a"
-//! BeethovenElise melody
+// BeethovenElise melody
 static const uint16_t skMelodyBeethovenelise[] PROGMEM =
 {
     TONE_PAUSE, 60, TONE_NOTE_E6, 240, TONE_NOTE_DS5, 240, TONE_NOTE_E6, 240, TONE_NOTE_DS5, 240,
@@ -92,6 +94,9 @@ static const uint16_t skMelodyBeethovenelise[] PROGMEM =
     TONE_NOTE_B5, 720, TONE_NOTE_D5, 240, TONE_NOTE_C6, 240, TONE_NOTE_B5, 240, TONE_NOTE_A5, 960,
     TONE_END,
 };
+
+#endif // !defined(__DOXYGEN__)
+
 
 static void sAppTask(void *pArg)
 {

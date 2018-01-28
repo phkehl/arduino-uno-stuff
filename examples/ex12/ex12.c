b92ed72bfa33322df14f40cd189ed038537fa968
Philippe Kehl <phkehl@gmail.com>
2018-01-28 14:18:33 +0100

    ex12: reliable rotary encoder and button detection

--- examples/ex12/ex12.c
+++ examples/ex12/ex12.c
@@ -6,9 +6,7 @@
 
     \addtogroup EXAMPLES_EX12
 
-    It assumes an encoder with detents and hence there is no resolution to loos when it only uses
-    the falling edge of one pin and sampling the other pin. The interrupt pin has to be debounced
-    (100nF or so between pin and ground).
+    The interrupt pin should be debounced (100nF or so between pin and ground) to minimise ISR load.
 
     @{
 */
@@ -32,40 +30,67 @@ typedef enum EVENT_e
 {
     EVENT_CNT_INC,
     EVENT_CNT_DEC,
-    EVENT_SGL_PRESS,
-    EVENT_DBL_PRESS,
-    EVENT_LNG_PRESS,
+    EVENT_BTN,
 } EVENT_t;
 
 #define EVENT_QUEUE_LEN 10
 
 static OS_QUEUE_t sEventQueue;
 
+static volatile uint32_t svEventLastMsss;
+
+#define EVENT_MIN_DT 20
+
 
 /* ***** rotary encoder ****************************************************** */
+
+//  clock-wise (increment):                  anti clock-wise (decrement):
+//
+//           1         2                          1         2
+//   --------+         +---------             ----+         +-------------
+//           |<...d...>|                          |<...d...>|
+//           |         |                          |         |
+//           +---------+                          +---------+
+//                     .                                    .
+//   ----+         +---o---------             --------+     .   +---------
+//       |         |                                  |     .   |
+//       |         |                                  |     .   |
+//       +---------+                                  +-----o---+
+//
+//
+//  1 = ISR fires as we start rotating (falling edge)
+//  2 = ISR fires again as it locks into the next position (raising edge)
+//  d = we only accept the reading if d > ROTENC_MIN_DN_TIME
+//  o = if it is, we sample the second signal here and determine the direction
+//
 
 #define ROTENC_PIN1 _PD2 // pin 1 (must be PD2 = INT0)
 #define ROTENC_PIN2 _PD4 // pin 2
 
-typedef enum ROTENC_DIR_e
-{
-    ROTENC_INC,
-    ROTENC_DEC,
-} ROTENC_DIR_t;
+static volatile uint32_t svRotEncDnMsss;
 
-//static volatile uint32_t svRotEncLast;
-//static volatile uint16_t svRotEncDt;
+#define ROTENC_MIN_DN_TIME 5
 
 ISR(INT0_vect) // external interrupt 0
 {
     osIsrEnter();
 
-    const EVENT_t ev = PIN_GET(ROTENC_PIN2) ? EVENT_CNT_DEC : EVENT_CNT_INC;
-    osQueueSend(&sEventQueue, &ev, -1);
-
-//    const uint32_t msss = osTaskGetMsss();
-//    svRotEncDt = msss - svRotEncLast;
-//    svRotEncLast = msss;
+    // down
+    if (!PIN_GET(ROTENC_PIN1))
+    {
+        svRotEncDnMsss = osTaskGetTicks();
+    }
+    // up (snap in)
+    else
+    {
+        const uint32_t msss = osTaskGetTicks();
+        if (svRotEncDnMsss && ((msss - svRotEncDnMsss) > ROTENC_MIN_DN_TIME))
+        {
+            const EVENT_t ev = PIN_GET(ROTENC_PIN2) ? EVENT_CNT_INC : EVENT_CNT_DEC;
+            osQueueSend(&sEventQueue, &ev, -1);
+        }
+        svRotEncDnMsss = 0;
+    }
 
     osIsrLeave();
 }
@@ -75,23 +100,30 @@ ISR(INT0_vect) // external interrupt 0
 
 #define BUTTON_PIN  _PD3 // (must be PD3 = INT1)
 
-#define BUTTON_PRESS_TIME 150
+#define BUTTON_MIN_DN_TIME 20 // minumum time button must be pressed down
 
-static volatile uint8_t svButtonCnt;
+static volatile uint32_t svButtonDnMsss;
 
 ISR(INT1_vect) // external interrupt 1
 {
     osIsrEnter();
 
-    static volatile uint32_t msss0;
-    const uint32_t msss1 = osTaskGetTicks();
-    if ( (msss1 - msss0) >= BUTTON_PRESS_TIME )
+    // button down
+    if (!PIN_GET(BUTTON_PIN))
     {
-        const EVENT_t ev = EVENT_SGL_PRESS;
-        osQueueSend(&sEventQueue, &ev, -1);
-        svButtonCnt++;
+        svButtonDnMsss = osTaskGetTicks();
     }
-    msss0 = msss1;
+    // button up
+    else
+    {
+        const uint32_t msss = osTaskGetTicks();
+        if (svButtonDnMsss && ((msss - svButtonDnMsss) > BUTTON_MIN_DN_TIME))
+        {
+            const EVENT_t ev = EVENT_BTN;
+            osQueueSend(&sEventQueue, &ev, -1);
+        }
+        svButtonDnMsss = 0;
+    }
 
     osIsrLeave();
 }
@@ -109,31 +141,30 @@ void appInit(void)
 {
     DEBUG("ex12: init");
 
+    // event queue
+    static EVENT_t sEventQueueBuf[EVENT_QUEUE_LEN];
+    osQueueCreate(&sEventQueue, &sEventQueueBuf, EVENT_QUEUE_LEN, sizeof(EVENT_t));
+
     // rotary encoder
     PIN_INPUT(ROTENC_PIN1);            // signals
     PIN_PULLUP_ON(ROTENC_PIN1);
     PIN_INPUT(ROTENC_PIN2);
     PIN_PULLUP_ON(ROTENC_PIN2);
-    SETBITS(EICRA, BIT(ISC01));        // falling-edge..
-    CLRBITS(EICRA, BIT(ISC00));        // ..triggers interrupt
+    CLRBITS(EICRA, BIT(ISC01));        // any change in logic level..
+    SETBITS(EICRA, BIT(ISC00));        // ..triggers interrupt
     SETBITS(EIMSK, BIT(INT0));         // enable INT0
     SETBITS(EIFR, BIT(INTF0));         // clear INT0
 
     // button
     PIN_INPUT(BUTTON_PIN);
     PIN_PULLUP_ON(BUTTON_PIN);
-    SETBITS(EICRA, BIT(ISC11));        // falling-edge..
-    CLRBITS(EICRA, BIT(ISC10));        // ..triggers interrupt
+    CLRBITS(EICRA, BIT(ISC11));        // any change in logic level..
+    SETBITS(EICRA, BIT(ISC10));        // ..triggers interrupt
     SETBITS(EIMSK, BIT(INT1));         // enable INT1
     SETBITS(EIFR, BIT(INTF1));         // clear INT1
 
-    // event queue
-    static EVENT_t sEventQueueBuf[EVENT_QUEUE_LEN];
-    osQueueCreate(&sEventQueue, &sEventQueueBuf, EVENT_QUEUE_LEN, sizeof(EVENT_t));
-
     // register status function for the system task
     sysRegisterMonFunc(sAppStatus);
-
 }
 
 // starts the user application task
@@ -153,6 +184,9 @@ static void sAppTask(void *pArg)
     // not using the task argument
     UNUSED(pArg);
 
+    // clear event queue
+    osQueueClear(&sEventQueue);
+
     static int16_t count;
 
     while (ENDLESS)
@@ -170,14 +204,8 @@ static void sAppTask(void *pArg)
                     DEBUG("DEC %"PRIu16, count);
                     count--;
                     break;
-                case EVENT_SGL_PRESS:
-                    DEBUG("SGL %"PRIu8, svButtonCnt);
-                    break;
-                case EVENT_DBL_PRESS:
-                    DEBUG("DBL");
-                    break;
-                case EVENT_LNG_PRESS:
-                    DEBUG("LNG");
+                case EVENT_BTN:
+                    DEBUG("BTN");
                     break;
             }
         }

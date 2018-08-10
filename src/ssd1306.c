e62f93d563d6cd40a62686caf73f04a8419d2c5a
Philippe Kehl <phkehl@gmail.com>
2018-08-11 00:30:13 +0200

    wikipedia knows it all.. :)

--- src/ssd1306.c
+++ src/ssd1306.c
@@ -231,7 +231,62 @@ void ssd1306LineV(uint16_t x, uint16_t y, uint16_t h, const bool colour)
 
 void ssd1306Line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, const bool colour)
 {
-    // TODO
+    // TODO: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+    /*
+plotLineLow(x0,y0, x1,y1)
+  dx = x1 - x0
+  dy = y1 - y0
+  yi = 1
+  if dy < 0
+    yi = -1
+    dy = -dy
+  end if
+  D = 2*dy - dx
+  y = y0
+
+  for x from x0 to x1
+    plot(x,y)
+    if D > 0
+       y = y + yi
+       D = D - 2*dx
+    end if
+    D = D + 2*dy
+
+plotLineHigh(x0,y0, x1,y1)
+  dx = x1 - x0
+  dy = y1 - y0
+  xi = 1
+  if dx < 0
+    xi = -1
+    dx = -dx
+  end if
+  D = 2*dx - dy
+  x = x0
+
+  for y from y0 to y1
+    plot(x,y)
+    if D > 0
+       x = x + xi
+       D = D - 2*dy
+    end if
+    D = D + 2*dx
+
+plotLine(x0,y0, x1,y1)
+  if abs(y1 - y0) < abs(x1 - x0)
+    if x0 > x1
+      plotLineLow(x1, y1, x0, y0)
+    else
+      plotLineLow(x0, y0, x1, y1)
+    end if
+  else
+    if y0 > y1
+      plotLineHigh(x1, y1, x0, y0)
+    else
+      plotLineHigh(x0, y0, x1, y1)
+    end if
+  end if
+
+     */
 }
 
 void ssd1306Rect(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, const bool colour)

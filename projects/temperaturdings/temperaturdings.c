c5baf62f6ec494d8c5d5e6ae815eea1773a24867
Philippe Kehl <phkehl@gmail.com>
2018-08-19 19:12:55 +0200

    temperaturdings: add sequence of interesting stuff to display

--- projects/temperaturdings/temperaturdings.c
+++ projects/temperaturdings/temperaturdings.c
@@ -76,49 +76,49 @@ void appCreateTask(void)
 static uint32_t sMeasCnt = 0;
 
 // latest measurement
-static uint16_t sLastMeasH = SI7021_NODATA;
-static uint16_t sLastMeasT = SI7021_NODATA;
+static int16_t sLastMeasH = SI7021_NODATA;
+static int16_t sLastMeasT = SI7021_NODATA;
 static uint32_t sLastMeasTime = 0;
 
 // measurements of the last minute
 #define MEAS_PER_MIN (60 * (uint16_t)1000 / MEAS_PERIOD)
-static uint16_t sMinuteMeasH[MEAS_PER_MIN];
-static uint16_t sMinuteMeasT[MEAS_PER_MIN];
-static uint16_t sMinuteMeasIx;
-static uint16_t sMinuteAvgH;
-static uint16_t sMinuteMinH;
-static uint16_t sMinuteMaxH;
-static uint16_t sMinuteAvgT;
-static uint16_t sMinuteMinT;
-static uint16_t sMinuteMaxT;
+static int16_t sMinuteMeasT[MEAS_PER_MIN];
+static int16_t sMinuteMeasH[MEAS_PER_MIN];
+static int16_t sMinuteMeasIx;
+static int16_t sMinuteAvgT;
+static int16_t sMinuteMinT;
+static int16_t sMinuteMaxT;
+static int16_t sMinuteAvgH;
+static int16_t sMinuteMinH;
+static int16_t sMinuteMaxH;
 
 // minute measurements of last last hour
 #define MIN_PER_HOUR 60
-static uint16_t sHourMeasH[MIN_PER_HOUR];
-static uint16_t sHourMeasT[MIN_PER_HOUR];
-static uint16_t sHourMeasIx;
-static uint16_t sHourAvgH;
-static uint16_t sHourMinH;
-static uint16_t sHourMaxH;
-static uint16_t sHourAvgT;
-static uint16_t sHourMinT;
-static uint16_t sHourMaxT;
+static int16_t sHourMeasT[MIN_PER_HOUR];
+static int16_t sHourMeasH[MIN_PER_HOUR];
+static int16_t sHourMeasIx;
+static int16_t sHourAvgT;
+static int16_t sHourMinT;
+static int16_t sHourMaxT;
+static int16_t sHourAvgH;
+static int16_t sHourMinH;
+static int16_t sHourMaxH;
 
 // hour measurements of last last day
 #define HOUR_PER_DAY 24
-static uint16_t sDayMeasH[HOUR_PER_DAY];
-static uint16_t sDayMeasT[HOUR_PER_DAY];
-static uint16_t sDayMeasIx;
-static uint16_t sDayAvgH;
-static uint16_t sDayMinH;
-static uint16_t sDayMaxH;
-static uint16_t sDayAvgT;
-static uint16_t sDayMinT;
-static uint16_t sDayMaxT;
+static int16_t sDayMeasT[HOUR_PER_DAY];
+static int16_t sDayMeasH[HOUR_PER_DAY];
+static int16_t sDayMeasIx;
+static int16_t sDayAvgT;
+static int16_t sDayMinT;
+static int16_t sDayMaxT;
+static int16_t sDayAvgH;
+static int16_t sDayMinH;
+static int16_t sDayMaxH;
 
 static void sInitStats(void)
 {
-    uint16_t ix;
+    uint8_t ix;
 
     ix = NUMOF(sMinuteMeasH);
     while (ix > 0)
@@ -192,21 +192,21 @@ static void sUpdateStats(void)
 
     // calculate last minute's average
     {
-        uint32_t sumH = 0;
-        uint16_t numH = 0;
-        uint16_t minH = UINT16_MAX;
-        uint16_t maxH = 0;
+        int32_t sumH = 0;
+        int16_t numH = 0;
+        int16_t minH = INT16_MAX;
+        int16_t maxH = INT16_MIN;
 
-        uint32_t sumT = 0;
-        uint16_t numT = 0;
-        uint16_t minT = UINT16_MAX;
-        uint16_t maxT = 0;
+        int32_t sumT = 0;
+        int16_t numT = 0;
+        int16_t minT = INT16_MAX;
+        int16_t maxT = INT16_MIN;
 
-        uint16_t measIx = NUMOF(sMinuteMeasH);
+        int16_t measIx = NUMOF(sMinuteMeasH);
         while (measIx > 0)
         {
             measIx--;
-            const uint16_t measH = sMinuteMeasH[measIx];
+            const int16_t measH = sMinuteMeasH[measIx];
             if (measH != SI7021_NODATA)
             {
                 sumH += measH;
@@ -214,7 +214,7 @@ static void sUpdateStats(void)
                 if (measH > maxH) { maxH = measH; }
                 if (measH < minH) { minH = measH; }
             }
-            const uint16_t measT = sMinuteMeasT[measIx];
+            const int16_t measT = sMinuteMeasT[measIx];
             if (measT != SI7021_NODATA)
             {
                 sumT += measT;
@@ -276,21 +276,21 @@ static void sUpdateStats(void)
 
     // calculate last hours's average
     {
-        uint32_t sumH = 0;
-        uint16_t numH = 0;
-        uint16_t minH = UINT16_MAX;
-        uint16_t maxH = 0;
+        int32_t sumH = 0;
+        int16_t numH = 0;
+        int16_t minH = INT16_MAX;
+        int16_t maxH = INT16_MIN;
 
-        uint32_t sumT = 0;
-        uint16_t numT = 0;
-        uint16_t minT = UINT16_MAX;
-        uint16_t maxT = 0;
+        int32_t sumT = 0;
+        int16_t numT = 0;
+        int16_t minT = INT16_MAX;
+        int16_t maxT = INT16_MIN;
 
         uint16_t measIx = NUMOF(sHourMeasH);
         while (measIx > 0)
         {
             measIx--;
-            const uint16_t measH = sHourMeasH[measIx];
+            const int16_t measH = sHourMeasH[measIx];
             if (measH != SI7021_NODATA)
             {
                 sumH += measH;
@@ -298,7 +298,7 @@ static void sUpdateStats(void)
                 if (measH > maxH) { maxH = measH; }
                 if (measH < minH) { minH = measH; }
             }
-            const uint16_t measT = sHourMeasT[measIx];
+            const int16_t measT = sHourMeasT[measIx];
             if (measT != SI7021_NODATA)
             {
                 sumT += measT;
@@ -354,21 +354,21 @@ static void sUpdateStats(void)
 
     // calculate last 24 hours (day's) average
     {
-        uint32_t sumH = 0;
-        uint16_t numH = 0;
-        uint16_t minH = UINT16_MAX;
-        uint16_t maxH = 0;
+        int32_t sumH = 0;
+        int16_t numH = 0;
+        int16_t minH = INT16_MAX;
+        int16_t maxH = INT16_MIN;
 
-        uint32_t sumT = 0;
-        uint16_t numT = 0;
-        uint16_t minT = UINT16_MAX;
-        uint16_t maxT = 0;
+        int32_t sumT = 0;
+        int16_t numT = 0;
+        int16_t minT = INT16_MAX;
+        int16_t maxT = INT16_MIN;
 
-        uint16_t measIx = NUMOF(sDayMeasH);
+        int16_t measIx = NUMOF(sDayMeasH);
         while (measIx > 0)
         {
             measIx--;
-            const uint16_t measH = sDayMeasH[measIx];
+            const int16_t measH = sDayMeasH[measIx];
             if (measH != SI7021_NODATA)
             {
                 sumH += measH;
@@ -376,7 +376,7 @@ static void sUpdateStats(void)
                 if (measH > maxH) { maxH = measH; }
                 if (measH < minH) { minH = measH; }
             }
-            const uint16_t measT = sDayMeasT[measIx];
+            const int16_t measT = sDayMeasT[measIx];
             if (measT != SI7021_NODATA)
             {
                 sumT += measT;
@@ -425,80 +425,225 @@ static void sDisplayHello(void)
     gfxUpdate();
 }
 
-static void sDisplayData(void)
+static int16_t sRenderHead(const char *title, const uint32_t count)
+{
+    int16_t y = 0, x = 0;
+
+    // title
+    gfxPrint_P(GFX_FONT_5X7_1, 0, y, GFX_BLACK, GFX_TRANS, title);
+
+    // count
+    const uint8_t strLen = strlen_P(title);
+    const int16_t strWidth = (5 + 1) * strLen;
+    const uint8_t num = (gfxWidth() - strWidth) / 6;
+    static char cStr[12];
+    switch (num)
+    {
+        case 11: snprintf_P(cStr, sizeof(cStr), PSTR("%11"PRIu32), count); break;
+        case 10: snprintf_P(cStr, sizeof(cStr), PSTR("%10"PRIu32), count); break;
+        case  9: snprintf_P(cStr, sizeof(cStr), PSTR("%9"PRIu32),  count %   999999999); break;
+        case  8: snprintf_P(cStr, sizeof(cStr), PSTR("%8"PRIu32),  count %    99999999); break;
+        case  7: snprintf_P(cStr, sizeof(cStr), PSTR("%7"PRIu32),  count %     9999999); break;
+        case  6: snprintf_P(cStr, sizeof(cStr), PSTR("%6"PRIu32),  count %      999999); break;
+        case  5: snprintf_P(cStr, sizeof(cStr), PSTR("%5"PRIu32),  count %       99999); break;
+        case  4: snprintf_P(cStr, sizeof(cStr), PSTR("%4"PRIu32),  count %        9999); break;
+        case  3: snprintf_P(cStr, sizeof(cStr), PSTR("%3"PRIu32),  count %         999); break;
+        case  2: snprintf_P(cStr, sizeof(cStr), PSTR("%2"PRIu32),  count %          99); break;
+        case  1: snprintf_P(cStr, sizeof(cStr), PSTR("%1"PRIu32),  count %           9); break;
+        default: cStr[0] = '\0'; break;
+    }
+
+    if (num > 0)
+    {
+        x = gfxWidth() - (num * 6) + 1;
+        gfxPrint(GFX_FONT_5X7_1, x, y, GFX_BLACK, GFX_TRANS, cStr);
+    }
+
+    y += 7 + 1;
+    //gfxLineH(0, y, gfxWidth() - 1, GFX_BLACK);
+    for (x = 0; x < gfxWidth(); x += 2)
+    {
+        gfxPixel(x, y, GFX_BLACK);
+    }
+    y += 1;
+
+    return y;
+}
+
+static void sFormatMeas(char *str, const uint8_t size, int16_t meas)
+{
+    if (size > 5)
+    {
+        if (meas == SI7021_NODATA)
+        {
+            strcpy_P(str, PSTR("?????"));
+        }
+        else
+        {
+            // limit display to 5 chars (from "-9.99" to "99.99")
+            const float val = (float)CLIP(meas, -999, 9999) * 0.01f;
+            snprintf_P(str, size, PSTR("%.2f"), val);
+        }
+    }
+    else
+    {
+        *str = '\0';
+    }
+}
+
+static int16_t sRenderMeas(const int16_t y, char label, const int16_t meas)
+{
+    char str[10];
+
+    str[0] = label;
+    str[1] = '\0';
+    gfxPrint(GFX_FONT_5X7_1,  0, y + 7, GFX_BLACK, GFX_TRANS, str);
+
+    sFormatMeas(str, sizeof(str), meas);
+    const char frac = str[4];
+    str[4] = '\0';
+    gfxPrint(GFX_FONT_5X7_2, 10, y,     GFX_BLACK, GFX_TRANS, str);
+
+    str[0] = frac;
+    str[1] = '\0';
+    gfxPrint(GFX_FONT_5X7_1, 58, y + 7, GFX_BLACK, GFX_TRANS, str);
+
+    return y + (2 * 7);
+}
+
+static void sDisplayMeas(void)
 {
     gfxClear();
-    static char strH[10];
-    static char strT[10];
-    uint8_t fracH, fracT;
-    bool haveData = false;
+
+    int16_t measT, measH;
     if (osMutexClaim(&sMeasMx, 50))
     {
-        haveData = true;
-        if (sLastMeasH != SI7021_NODATA)
-        {
-            snprintf_P(strH, sizeof(strH), PSTR("%.2f"), (float)sLastMeasH * 0.01f);
-            fracH = sLastMeasH % 10;
-        }
-        else
-        {
-            strcpy_P(strH, PSTR("fail"));
-            fracH = 0;
-        }
-        if (sLastMeasT != SI7021_NODATA)
-        {
-            snprintf_P(strT, sizeof(strT), PSTR("%.2f"), (float)sLastMeasT * 0.01f);
-            fracT = sLastMeasT % 10;
-        }
-        else
-        {
-            strcpy_P(strT, PSTR("fail"));
-            fracT = 0;
-        }
+        measT = sLastMeasT;
+        measH = sLastMeasH;
+
         osMutexRelease(&sMeasMx);
     }
     else
     {
         WARNING("meas mx (disp)");
-        fracH = 0;
-        fracT = 0;
-        strcpy_P(strH, PSTR("????"));
-        strcpy_P(strT, PSTR("????"));
-    }
 
-    // strip last digit (xx.xx --> xx.x)
-    strH[4] = '\0'; // FIXME: can be 100.00
-    strT[4] = '\0';
+        measT = SI7021_NODATA;
+        measH = SI7021_NODATA;
+    }
 
     // render things
-    int16_t y = 0;
+    int16_t y;
+    y = sRenderHead(PSTR("now"), sMeasCnt);
+    y = sRenderMeas(y + 5, 'T', measT);
+    y = sRenderMeas(y + 5, 'H', measH);
 
-    // humidty
-    gfxPrint(GFX_FONT_5X7_1,  0,  y + 7, GFX_BLACK, GFX_TRANS, "R");
-    gfxPrint(GFX_FONT_5X7_2, 10,  y,     GFX_BLACK, GFX_TRANS, strH);
-    if (haveData)
-    {
-        char str[2] = { '0' + fracH, '\0' };
-        gfxPrint(GFX_FONT_5X7_1, 58, y + 7, GFX_BLACK, GFX_TRANS, str);
-    }
-    y += (2 * 7);
+    gfxUpdate();
+}
 
-    // temperature
-    y += 2;
-    gfxPrint(GFX_FONT_5X7_1,  0, y + 7, GFX_BLACK, GFX_TRANS, "T");
-    gfxPrint(GFX_FONT_5X7_2, 10, y,     GFX_BLACK, GFX_TRANS, strT);
-    if (haveData)
-    {
-        char str[2] = { '0' + fracT, '\0' };
-        gfxPrint(GFX_FONT_5X7_1, 58, y + 7, GFX_BLACK, GFX_TRANS, str);
-    }
-    y += (2 * 7);
+static void sDisplayMinute(void)
+{
+    gfxClear();
 
-    // measurement count
-    y = gfxHeight() - 1 - 7;
-    static char cStr[10];
-    snprintf_P(cStr, sizeof(cStr), PSTR("%"PRIu32), sMeasCnt);
-    gfxPrint(GFX_FONT_5X7_1, 0, y, GFX_BLACK, GFX_TRANS, cStr);
+    // render things
+    int16_t y;
+    y = sRenderHead(PSTR("minute"), sMinuteMeasIx);
+    y = sRenderMeas(y + 5, 'T', sMinuteAvgT);
+    y = sRenderMeas(y + 5, 'H', sMinuteAvgH);
+
+    gfxUpdate();
+}
+
+static void sDisplayHour(void)
+{
+    gfxClear();
+
+    // render things
+    int16_t y;
+    y = sRenderHead(PSTR("hour"), sHourMeasIx);
+    y = sRenderMeas(y + 5, 'T', sHourAvgT);
+    y = sRenderMeas(y + 5, 'H', sHourAvgH);
+
+    gfxUpdate();
+}
+
+static void sDisplayDay(void)
+{
+    gfxClear();
+
+    // render things
+    int16_t y;
+    y = sRenderHead(PSTR("day"), sDayMeasIx);
+    y = sRenderMeas(y + 5, 'T', sDayAvgT);
+    y = sRenderMeas(y + 5, 'H', sDayAvgH);
+
+    gfxUpdate();
+}
+
+static int16_t sRenderStats(
+    const int16_t y, const char label, const int16_t avg, const int16_t min, const int16_t max)
+{
+    char str[10];
+
+    str[0] = label;
+    str[1] = '\0';
+
+    gfxPrint(GFX_FONT_5X7_1, 0, y, GFX_BLACK, GFX_TRANS, str);
+
+    sFormatMeas(str, sizeof(str), avg);
+    gfxPrint(GFX_FONT_5X7_1, 20, y, GFX_BLACK, GFX_TRANS, str);
+
+    sFormatMeas(str, sizeof(str), min);
+    str[4] = '\0';
+    gfxPrint(GFX_FONT_5X7_1, 7, y + 7 + 2, GFX_BLACK, GFX_TRANS, str);
+
+    sFormatMeas(str, sizeof(str), max);
+    str[4] = '\0';
+    gfxPrint(GFX_FONT_5X7_1, gfxWidth() / 2 + 7, y + 7 + 2, GFX_BLACK, GFX_TRANS, str);
+
+    return y + 7 + 2 + 7 + 1;
+}
+
+static void sDisplayStatsMinute(void)
+{
+    gfxClear();
+
+    // render things
+    int16_t y;
+
+    y = sRenderHead(PSTR("stats (min)"), 0);
+
+    y = sRenderStats(y + 3, 'T', sMinuteAvgT, sMinuteMinT, sMinuteMaxT);
+    y = sRenderStats(y + 3, 'H', sMinuteAvgH, sMinuteMinH, sMinuteMaxH);
+
+    gfxUpdate();
+}
+
+static void sDisplayStatsHour(void)
+{
+    gfxClear();
+
+    // render things
+    int16_t y;
+
+    y = sRenderHead(PSTR("stats (hr)"), 0);
+
+    y = sRenderStats(y + 3, 'T', sMinuteAvgT, sMinuteMinT, sMinuteMaxT);
+    y = sRenderStats(y + 3, 'H', sMinuteAvgH, sMinuteMinH, sMinuteMaxH);
+
+    gfxUpdate();
+}
+
+static void sDisplayStatsDay(void)
+{
+    gfxClear();
+
+    // render things
+    int16_t y;
+
+    y = sRenderHead(PSTR("stats (day)"), 0);
+
+    y = sRenderStats(y + 3, 'T', sMinuteAvgT, sMinuteMinT, sMinuteMaxT);
+    y = sRenderStats(y + 3, 'H', sMinuteAvgH, sMinuteMinH, sMinuteMaxH);
 
     gfxUpdate();
 }
@@ -506,6 +651,13 @@ static void sDisplayData(void)
 /* ***** application task **************************************************** */
 
 static uint32_t sAppCnt = 0;
+
+
+// display sequence
+// (c = hello, x = meas., . = no update, _ = clear, m/h/d = avg minute/hour/day, M/H/D = stats minute/hour/day)
+//                                                 1         2         3         4         5         6         7         8
+//                                        12345678901234567890123456789012345678901234567890123456789012345678901234567890
+const __flash char skDisplaySequence[] = "ccxxxxxxxxxxmmhhddxxxxxxxxxxMMHHDDxxxxxxxxxx";
 
 // application task
 static void sAppTask(void *pArg)
@@ -535,11 +687,8 @@ static void sAppTask(void *pArg)
         // do measurement and update statistics
         if (osMutexClaim(&sMeasMx, 500))
         {
-            DEBUG("m");
             sMeasurement();
-            DEBUG("u");
             sUpdateStats();
-            DEBUG("d");
 
             osMutexRelease(&sMeasMx);
         }
@@ -550,13 +699,32 @@ static void sAppTask(void *pArg)
             hwPanic(HW_PANIC_OTHER, 0, 0);
         }
 
-        if ((sAppCnt % 10) < 2)
+        // update display
+        static uint8_t seqIx;
+        const char seqChar = pgm_read_byte(&skDisplaySequence[seqIx]);
+        //DEBUG("seqIx=%"PRIu8" seqChar=%c", seqIx, seqChar);
+        seqIx++;
+        seqIx %= (sizeof(skDisplaySequence) - 1);
+
+        switch (seqChar)
         {
-            sDisplayHello();
-        }
-        else
-        {
-            sDisplayData();
+            case 'c': sDisplayHello();         break;
+
+            case 'x': sDisplayMeas();          break;
+
+            case 'm': sDisplayMinute();        break;
+            case 'h': sDisplayHour();          break;
+            case 'd': sDisplayDay();           break;
+
+            case 'M': sDisplayStatsMinute();   break;
+            case 'H': sDisplayStatsHour();     break;
+            case 'D': sDisplayStatsDay();      break;
+
+            case '_': gfxClear(); gfxUpdate(); break;
+
+            case '.':
+            default:
+                break;
         }
     }
 }
